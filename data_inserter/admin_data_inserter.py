71f19f4ce47f2af97039f0bfc7c1e6dfa8938477
dineshtavva23 <cs24btech11061@iith.ac.in>
2025-04-21 18:47:17 +0530

    files to add admin and student data to mongodb database

--- data_inserter/admin_data_inserter.py
+++ data_inserter/admin_data_inserter.py
@@ -1,1 +1,25 @@
+from pymongo import MongoClient
 
+client = MongoClient("mongodb+srv://dinesh_23:tc97386@cluster0.7a7ovpk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+db = client["mess"]
+collection_admin = db["admins"]
+
+data_admin = [
+    {
+    "name" : "Aditya Kumar",
+    "mess" : "MESS-A",
+    "position": "Admin",
+    "email" : "aditya@cse.iith.ac.in",
+    "passwd": "aditya95"
+},
+{
+    "name": "Saurabh",
+    "mess": "MESS-B",
+    "position": "Admin",
+    "email" : "saurabh@ph.iith.ac.in",
+    "passwd": "saurabhep06"
+}
+]
+
+collection_admin.insert_many(data_admin)
+print("Inserted admin data into database")
